504dba10412604111ce38882dc17f18a44ed8537
yangwei <897900564@qq.com>
2022-07-21 09:13:34 +0800

    提交一下

--- spark-demo/src/main/scala/com/yw/spark/example/sql/cases/Case14_DataFromMysqlPlan.scala
+++ spark-demo/src/main/scala/com/yw/spark/example/sql/cases/Case14_DataFromMysqlPlan.scala
@@ -1,7 +1,9 @@
 package com.yw.spark.example.sql.cases
 
+import java.util.Properties
+
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 /**
   *
@@ -16,7 +18,44 @@ object Case14_DataFromMysqlPlan {
     val spark = SparkSession.builder().config(conf).getOrCreate()
     spark.sparkContext.setLogLevel("WARN")
 
-    // 3. 读取 MySQL 表中的数据
+    // 3. 读取 MySQL 表
+    val url = "jdbc:mysql://192.168.254.132:3306/mydb?characterEncoding=UTF-8"
+    val props = new Properties()
+    props.setProperty("user", "root")
+    props.setProperty("password", "123456")
+    val studentDF: DataFrame = spark.read.jdbc(url, "students", props)
+    val scoreDF: DataFrame = spark.read.jdbc(url, "scores", props)
 
+    // 4. 将DataFrame注册成临时视图
+    studentDF.createTempView("students")
+    scoreDF.createTempView("scores")
+
+    // 5. 使用Spark SQL查询数据
+    val resultDF: DataFrame = spark.sql(
+      """
+        |SELECT tmp.class, SUM(tmp.degree), AVG(tmp.degree)
+        |FROM (
+        |	SELECT
+        |		students.sno AS ssno,
+        |		students.sname,
+        |		students.ssex,
+        |		students.sbirthday,
+        |		students.class,
+        |		scores.sno,
+        |		scores.degree,
+        |		scores.cno
+        |	FROM students
+        |	LEFT JOIN scores ON students.sno = scores.sno
+        |	WHERE degree > 60 AND sbirthday > '1973-01-01 00:00:00'
+        |) tmp GROUP BY tmp.class
+      """.stripMargin)
+
+    // 6. 查看执行计划
+    resultDF.explain(true)
+
+    // 7. 展示数据
+    resultDF.show()
+
+    spark.stop()
   }
 }
