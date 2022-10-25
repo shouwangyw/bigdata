d7062596cd8863a12ea778fd0f0b4fe6cb838b34
yangwei <897900564@qq.com>
2022-10-25 09:02:44 +0800

    造数

---
+++ log-collector/src/main/java/com/yw/log/collector/common/constant/LogType.java
@@ -0,0 +1,9 @@
+package com.yw.log.collector.common.constant;
+
+/**
+ * @author yangwei
+ */
+public class LogType {
+    public static final String LOG_TYPE_USER_PLAY_SONG = "userPlaySongLog";
+    public static final String LOG_TYPE_USER_LOGIN = "userLoginLog";
+}
---
+++ log-collector/src/test/java/com/yw/log/collector/ProduceUserLoginLog.java
@@ -0,0 +1,46 @@
+package com.yw.log.collector;
+
+import com.yw.log.collector.common.Constant;
+import com.yw.log.collector.common.constant.LogType;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.junit.platform.commons.util.StringUtils;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author yangwei
+ */
+public class ProduceUserLoginLog {
+    public static void main(String[] args) throws Exception {
+        HttpClient client = HttpClients.createDefault();
+        HttpPost post = new HttpPost(Constant.URL + LogType.LOG_TYPE_USER_LOGIN);
+        post.setHeader("Content-Type", "application/json");
+        while (true) {
+            try (InputStream is = ProduceUserLoginLog.class.getClassLoader().getResourceAsStream("user_login_infos");
+                 BufferedReader br = new BufferedReader(new InputStreamReader(Objects.requireNonNull(is)))) {
+                String line = br.readLine();
+                while (StringUtils.isNotBlank(line)) {
+                    post.setEntity(new StringEntity("["+line+"]", Charset.forName("UTF-8")));
+                    TimeUnit.MILLISECONDS.sleep(200);
+                    HttpResponse response = client.execute(post);
+                    if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                        final String result = EntityUtils.toString(response.getEntity(), "utf-8");
+                        System.out.println("==>> result: " + result);
+                    }
+                    line = br.readLine();
+                }
+            }
+        }
+    }
+}
