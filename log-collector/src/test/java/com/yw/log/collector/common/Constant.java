9fbfafa4afaed284c14078a0ecdc10368b859f12
yangwei <897900564@qq.com>
2022-10-26 21:46:00 +0800

    业务六：实时PVUV

--- log-collector/src/test/java/com/yw/log/collector/common/Constant.java
+++ log-collector/src/test/java/com/yw/log/collector/common/Constant.java
@@ -4,5 +4,6 @@ package com.yw.log.collector.common;
  * @author yangwei
  */
 public class Constant {
-    public static final String URL = "http://localhost:8686/collector/common/";
+//    public static final String URL = "http://localhost:8686/collector/common/";
+    public static final String URL = "http://node02:8686/collector/common/";
 }
