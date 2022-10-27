1ece69106b9d01b68134b0209d019964466edf12
yangwei <897900564@qq.com>
2022-10-27 09:13:39 +0800

    实时统计用户点播歌曲热度排行榜

--- music-warehouse/src/main/scala/com/yw/musichw/streaming/RealTimeHotSong.scala
+++ music-warehouse/src/main/scala/com/yw/musichw/streaming/RealTimeHotSong.scala
@@ -1,12 +1,24 @@
 package com.yw.musichw.streaming
 
+import com.alibaba.fastjson.JSON
+import com.yw.musichw.base.RedisClient
 import com.yw.musichw.util.ConfigUtils
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.TopicPartition
+import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.SparkContext
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.streaming.dstream.InputDStream
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.{ConsumerStrategies, HasOffsetRanges, KafkaUtils, OffsetRange}
 import org.apache.spark.streaming.{Durations, StreamingContext}
 
+import scala.collection.mutable
+
 /**
-  * 实时获取用户点播歌曲日志，每隔30秒，获取最近10分钟歌曲的点播热度，并将结果存入MySQL中。
+  * 实时获取用户点播歌曲日志，每隔10秒，获取最近10分钟歌曲的点播热度，并将结果存入MySQL中。
+  *
   * @author yangwei
   */
 object RealTimeHotSong {
@@ -39,6 +51,76 @@ object RealTimeHotSong {
     sc = sparkSession.sparkContext
     sc.setLogLevel("Error")
 
-    val ssc = new StreamingContext(sc, Durations.seconds(20))
+    val ssc = new StreamingContext(sc, Durations.seconds(10))
+    // 从redis中获取消费者offset
+    val currentTopicOffset: mutable.Map[String, String] = RedisClient.getOffsetFromRedis(redisOffsetDb, topicUserPlaySong)
+    currentTopicOffset.foreach(tp => println(s"初始读取到的topic offset: $tp"))
+    // 转换成需要的类型
+    val fromOffsets: Predef.Map[TopicPartition, Long] = currentTopicOffset.map {
+      resultSet => new TopicPartition(topicUserPlaySong, resultSet._1.toInt) -> resultSet._2.toLong
+    }.toMap
+
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> kafkaCluster,
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> "MyGroupId_1",
+      "auto.offset.reset" -> "latest",
+      "enable.auto.commit" -> (false: java.lang.Boolean) // 默认是true
+    )
+
+    // 将获取到的消费者offset 传递给SparkStreaming
+    val stream: InputDStream[ConsumerRecord[String, String]] = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      PreferConsistent,
+      ConsumerStrategies.Assign[String, String](fromOffsets.keys.toList, kafkaParams, fromOffsets)
+    )
+
+    // 实时统计热门歌曲
+    stream.map(cr => {
+      val jsonObject = JSON.parseObject(cr.value())
+      val songId = jsonObject.getString("songid")
+      val songName = jsonObject.getString("songname")
+      (songName, 1)
+    }).reduceByKeyAndWindow((v1: Int, v2: Int) => {
+      v1 + v2
+    }, Durations.minutes(10), Durations.seconds(10))
+        .foreachRDD(rdd => {
+          println("current is start ... ...")
+          // 将top30热度最高的歌曲，结果保存到MySQL表hot_song中
+          val hotSongInfo: RDD[HotSongInfo] = rdd.map(tp => {
+            HotSongInfo(tp._1, tp._2)
+          })
+          val session = sparkSession.newSession()
+          import session.implicits._
+          hotSongInfo.toDF().createTempView("temp_song_info")
+          session.sql(
+            """
+              |select songname, times, row_number() over (partition by 1 order by times desc) as rank
+              |from temp_song_info
+            """.stripMargin)
+              .filter("rank <= 30")
+              .write.format("jdbc")
+              .mode(SaveMode.Overwrite)
+              .option("url", mysqlUrl)
+              .option("user", mysqlUser)
+              .option("password", mysqlPassword)
+              .option("driver", "com.mysql.jdbc.Driver")
+              .option("dbtable", "hot_song")
+              .save()
+          println("current is finished ... ...")
+        })
+
+    stream.foreachRDD { (rdd: RDD[ConsumerRecord[String, String]]) =>
+      println("所有业务完成")
+      val offsetRanges: Array[OffsetRange] = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
+      // 将当前批次最后的所有分区offsets 保存到 Redis中
+      RedisClient.saveOffsetToRedis(redisOffsetDb, offsetRanges)
+    }
+
+    ssc.start()
+    ssc.awaitTermination()
+    ssc.stop()
   }
 }
+case class HotSongInfo(songName:String,times:Int)
