d195a749e5de36c65bb4dcf590068d659c9c8c6d
yangwei <897900564@qq.com>
2025-07-20 16:32:52 +0800

    机器学习相关案例代码

---
+++ machine-learning/src/main/scala/com/yw/lr/VectorTest.scala
@@ -0,0 +1,13 @@
+package com.yw.lr
+
+import org.apache.spark.ml.linalg.{DenseVector, SparseVector}
+
+object VectorTest {
+  def main(args: Array[String]): Unit = {
+    // 稀疏向量
+    val sparseVector = new SparseVector(6,Array(1,3,5),Array(1,1,1))
+    println(sparseVector)
+    val denseVector = new DenseVector(Array(1,1,0,1))
+    println(denseVector)
+  }
+}
